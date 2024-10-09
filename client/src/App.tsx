4c8e5aea7bddec2a30828295f8858be04f75c088
Jack Engelmann <engejack@gmail.com>
2024-10-09 19:51:31 +0200

    refactor App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,39 @@
 import './App.css';
-import { useSampleStream, useTaskConfig } from './api';
+import { Sample, TaskConfig, useSampleStream, useTaskConfig } from './api';
 import { ClassSelection } from './components/ClassSelection/ClassSelection';
 import { useState } from 'react';
 
 function App() {
+  // The samples are "streamed", so they are loaded one sample at the time.
+  // This allows a dynamic order of the samples. Your past annotations may
+  // influence which samples should be annotated next (e.g. active learning).
   const [samples, getNext, updateSample] = useSampleStream()
+
+  // This offset allows going back in the stream (e.g. user makes a mistake
+  // and wants to fix the previous annotation). The offset represents how many
+  // steps to go left from the tail of the stream.
   const [offset, setOffset] = useState(0)
   const taskConfig = useTaskConfig()
-  console.log(samples)
-  console.log(offset)
 
   if (!samples || samples.length === 0 || !taskConfig) {
     return <span>Loading</span>
   }
 
   const currentSample = samples[samples.length - 1 - offset]
-  let classPredictions: {className: string, value: number}[] = []
-  const textClassPredictions = currentSample.textClassPredictions
-  if (textClassPredictions) {
-    classPredictions = taskConfig.textClasses.map((className, classIndex) => ({
-      className,
-      value: textClassPredictions[classIndex]
-    })).sort((a, b) => b.value - a.value)
-  } else {
-    classPredictions = taskConfig.textClasses.map(n => ({ className: n, value: 0}))
-  }
+  const classPredictions = getSortedClassPredictions(currentSample, taskConfig)
   return (
     <div className="App">
       <div className="App__selector">
-        <button onClick={() => setOffset(offset => offset + 1)} disabled={offset >= samples.length - 1}>
+        <button
+          onClick={() => setOffset(offset => offset + 1)}
+          disabled={offset >= samples.length - 1}
+        >
           Previous
         </button>
-        <button onClick={() => setOffset(offset => offset - 1)} disabled={offset <= 0}>
+        <button
+          onClick={() => setOffset(offset => offset - 1)}
+          disabled={offset <= 0}
+        >
           Next
         </button>
       </div>
@@ -54,6 +56,20 @@ function App() {
       />
     </div>
   );
+}
+
+function getSortedClassPredictions(sample: Sample, taskConfig: TaskConfig): { className: string, value: number }[] {
+  if (!sample.textClassPredictions) {
+    return taskConfig.textClasses.map(className => ({ className, value: 0 }))
+  }
+
+  const textClassPredictions = taskConfig.textClasses.map((className, classIndex) => ({
+    className,
+    value: sample.textClassPredictions![classIndex]
+  }))
+
+  // Sort by value (confidence prediction) in descending order.
+  return textClassPredictions.sort((a, b) => b.value - a.value)
 }
 
 export default App;
